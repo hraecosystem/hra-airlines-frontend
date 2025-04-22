05aa932b3e99649271d82273154e23c9aaf0b254
hraweb3 <hraweb3@gmail.com>
2025-04-22 10:40:35 +0400

    Update .env and fix API base URL

--- lib/api.ts
+++ lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:4848/api/v1",
   withCredentials: true,    // ← send & receive httpOnly cookies
 });
 
